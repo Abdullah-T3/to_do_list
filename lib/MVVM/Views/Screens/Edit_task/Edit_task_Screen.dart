37e01a0019d1524336a373c32ad95a33ff46ee10
Omar <omarahmed2012006phone@gmail.com>
2025-01-08 20:28:02 +0200

    Quill Text

--- lib/MVVM/Views/Screens/Edit_task/Edit_task_Screen.dart
+++ lib/MVVM/Views/Screens/Edit_task/Edit_task_Screen.dart
@@ -10,6 +10,9 @@ import '../../../../theming/styles.dart';
 import '../../../VIew_Models/Task_View_Models/task_cubit.dart';
 import '../../Widgets/Add_Task_Widgets/InkWellWidget.dart';
 
+import 'package:flutter_quill/flutter_quill.dart' as quill;
+
+
 class EditTaskScreen extends StatelessWidget {
   EditTaskScreen({super.key, required this.task}) {
     // Initialize controllers with task data
@@ -31,8 +34,13 @@ class EditTaskScreen extends StatelessWidget {
   final TaskModel task;
   TaskModel? updatedTask;
 
+
   @override
   Widget build(BuildContext context) {
+
+    final quill.QuillController controller = quill.QuillController.basic();
+
+
     return Scaffold(
       appBar: AppBar(
         actions: [
@@ -72,105 +80,160 @@ class EditTaskScreen extends StatelessWidget {
           ),
         ],
         title: Text('Edit Task', style: TextStyles.appBarStyle),
-        backgroundColor: ColorsManager.buttonColor,
+        backgroundColor: ColorsManager.appBarColor,
       ),
       body: Infowidget(
         builder: (context, deviceinfo) {
           return Container(
-            color: ColorsManager.primaryColor,
-            child: Padding(
-              padding: EdgeInsetsDirectional.only(start: deviceinfo.screenWidth * 0.05, end: deviceinfo.screenWidth * 0.05, top: deviceinfo.screenHeight * 0.02, bottom: deviceinfo.screenHeight * 0.05),
-              child: BlocBuilder<TaskCubit, TaskState>(
-                builder: (context, state) {
-                  return SingleChildScrollView(
+
+            width: deviceinfo.screenWidth,
+            height: deviceinfo.screenHeight,
+            child: BlocBuilder<TaskCubit, TaskState>(
+              builder: (context, state) {
+                return SingleChildScrollView(
+                  child: Container(
+                    width: deviceinfo.screenWidth,
+                    height: deviceinfo.screenHeight,
+                    color: ColorsManager.EdittextFieldColor,
                     child: Column(
                       children: [
-                        TextField(
-                          decoration: TextFieldStyles.inputDecoration(deviceinfo: deviceinfo, hintText: 'Task Title'),
-                          controller: titleController,
-                        ),
-                        TextField(
-                          decoration: TextFieldStyles.inputDecoration(deviceinfo: deviceinfo, hintText: 'Place'),
-                          controller: placeController,
-                        ),
-                        InkWellWidget(
-                          OptionName: 'Repeat',
-                          InitialData: task.repeat ?? "One Time",
-                          OnTap: () async {
-                            await radioButtons(
-                              context: context,
-                              deviceinfo: deviceinfo,
-                              title: 'Repeat',
-                              ItemsList: [
-                                'One Time',
-                                'Daily',
-                                'Weekly',
-                                'Monthly',
-                              ],
-                            );
-                          },
-                        ),
-                        InkWellWidget(
-                          OptionName: 'Reminder',
-                          InitialData: "Before 5 Minutes",
-                          OnTap: () async {
-                            await radioButtons(
-                              context: context,
-                              deviceinfo: deviceinfo,
-                              title: 'Reminder',
-                              ItemsList: [
-                                'Before 5 Minutes',
-                                'Before 10 Minutes',
-                                'Before 15 Minutes',
-                                'Before 20 Minutes',
-                              ],
-                            );
-                          },
-                        ),
-                        InkWellWidget(
-                          OptionName: 'Start Date',
-                          InitialData: task.startDate ?? "",
-                          OnTap: () async {
-                            final DateTime? picked = await DatePicker(context);
-                            if (picked != null) {
-                              final formattedDate = "${picked.year}-${picked.month}-${picked.day}";
-                              dateController.text = formattedDate;
-                              context.read<TaskCubit>().changeDate(formattedDate);
-                            }
-                          },
-                        ),
-                        InkWellWidget(
-                          OptionName: 'Finish',
-                          InitialData: task.endDate ?? "",
-                          OnTap: () async {
-                            final DateTime? picked = await DatePicker(context);
-                            if (picked != null) {
-                              final formattedDate = "${picked.year}-${picked.month}-${picked.day}";
-                              task.endDate = formattedDate;
-                              context.read<TaskCubit>().changeDate(formattedDate);
-                            }
-                          },
-                        ),
-                        Container(
-                          height: deviceinfo.screenHeight * 0.55,
-                          width: deviceinfo.screenWidth * 0.8,
-                          padding: EdgeInsetsDirectional.only(start: deviceinfo.screenWidth * 0.03, end: deviceinfo.screenWidth * 0.03),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(deviceinfo.screenWidth * 0.05),
-                            color: ColorsManager.textFieldColor,
-                          ),
-                          child: TextField(
-                            decoration: const InputDecoration(
-                              border: InputBorder.none,
+                        // TextField(
+                        //   decoration: TextFieldStyles.inputDecoration(deviceinfo: deviceinfo, hintText: 'Task Title'),
+                        //   controller: titleController,
+                        // ),
+                        // TextField(
+                        //   decoration: TextFieldStyles.inputDecoration(deviceinfo: deviceinfo, hintText: 'Place'),
+                        //   controller: placeController,
+                        // ),
+                        // InkWellWidget(
+                        //   OptionName: 'Repeat',
+                        //   InitialData: task.repeat ?? "One Time",
+                        //   OnTap: () async {
+                        //     await radioButtons(
+                        //       context: context,
+                        //       deviceinfo: deviceinfo,
+                        //       title: 'Repeat',
+                        //       ItemsList: [
+                        //         'One Time',
+                        //         'Daily',
+                        //         'Weekly',
+                        //         'Monthly',
+                        //       ],
+                        //     );
+                        //   },
+                        // ),
+                        // InkWellWidget(
+                        //   OptionName: 'Reminder',
+                        //   InitialData: "Before 5 Minutes",
+                        //   OnTap: () async {
+                        //     await radioButtons(
+                        //       context: context,
+                        //       deviceinfo: deviceinfo,
+                        //       title: 'Reminder',
+                        //       ItemsList: [
+                        //         'Before 5 Minutes',
+                        //         'Before 10 Minutes',
+                        //         'Before 15 Minutes',
+                        //         'Before 20 Minutes',
+                        //       ],
+                        //     );
+                        //   },
+                        // ),
+                        // InkWellWidget(
+                        //   OptionName: 'Start Date',
+                        //   InitialData: task.startDate ?? "",
+                        //   OnTap: () async {
+                        //     final DateTime? picked = await DatePicker(context);
+                        //     if (picked != null) {
+                        //       final formattedDate = "${picked.year}-${picked.month}-${picked.day}";
+                        //       dateController.text = formattedDate;
+                        //       context.read<TaskCubit>().changeDate(formattedDate);
+                        //     }
+                        //   },
+                        // ),
+                        // InkWellWidget(
+                        //   OptionName: 'Finish',
+                        //   InitialData: task.endDate ?? "",
+                        //   OnTap: () async {
+                        //     final DateTime? picked = await DatePicker(context);
+                        //     if (picked != null) {
+                        //       final formattedDate = "${picked.year}-${picked.month}-${picked.day}";
+                        //       task.endDate = formattedDate;
+                        //       context.read<TaskCubit>().changeDate(formattedDate);
+                        //     }
+                        //   },
+                        // ),
+
+                        quill.QuillToolbar.simple(
+                            controller: controller,
+                            configurations: quill.QuillSimpleToolbarConfigurations(
+                              toolbarSize: deviceinfo.screenWidth * 0.115,
+                              showFontSize: true,
+                              showDividers: false,
+                              multiRowsDisplay: false,
+                              decoration: BoxDecoration(color: ColorsManager.textFieldColor,
+                                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(deviceinfo.screenWidth * 0.02), bottomRight: Radius.circular(deviceinfo.screenWidth * 0.02)),
+                                border:  BorderDirectional(bottom: BorderSide(width: deviceinfo.screenWidth * 0.017, color: Colors.grey[800]!)),
+                              ),
+
+                            )),
+
+
+
+                        // Container(
+                        //   height: deviceinfo.screenHeight,
+                        //   width: deviceinfo.screenWidth ,
+                        //   padding: EdgeInsetsDirectional.only(start: deviceinfo.screenWidth * 0.03, end: deviceinfo.screenWidth * 0.03),
+                        //   decoration: BoxDecoration(
+                        //     color: Color(0xff6015b2),
+                        //   ),
+                        //   child: TextField(
+                        //     decoration: const InputDecoration(
+                        //       border: InputBorder.none,
+                        //     ),
+                        //     controller: contentController,
+                        //   ),
+                        // ),
+
+                        Expanded(
+                          child: quill.QuillEditor.basic(
+                            controller: controller,
+                            configurations:  quill.QuillEditorConfigurations(
+                              padding: EdgeInsetsDirectional.only(start: deviceinfo.screenWidth * 0.03, end: deviceinfo.screenWidth * 0.03, top: deviceinfo.screenHeight * 0.02 , bottom: deviceinfo.screenHeight * 0.02),
+                              showCursor: true,
+                              placeholder: 'Enter your content here...', 
+                              customStyles: quill.DefaultStyles(
+
+                                placeHolder: quill.DefaultTextBlockStyle(
+                                    TextStyle(
+
+                                    color: Colors.grey,
+                                    fontSize: deviceinfo.screenWidth * 0.05
+                                  ),
+                                  quill.HorizontalSpacing(0, 0),
+                                  quill.VerticalSpacing(0, 0),
+                                  quill.VerticalSpacing(0, 0),
+                                  BoxDecoration()
+                                ),
+                                paragraph:quill.DefaultTextBlockStyle(
+                                  TextStyle(
+                                    color: Colors.white,
+                                    fontSize: deviceinfo.screenWidth * 0.05
+                                  ),
+                                    quill.HorizontalSpacing(0, 0),
+                                    quill.VerticalSpacing(0, 0),
+                                    quill.VerticalSpacing(0, 0),
+                                    BoxDecoration()
+                                ),
+                              )
                             ),
-                            controller: contentController,
                           ),
-                        ),
+                        )
                       ],
                     ),
-                  );
-                },
-              ),
+                  ),
+                );
+              },
             ),
           );
         },
--- lib/theming/colors.dart
+++ lib/theming/colors.dart
@@ -4,5 +4,9 @@ class ColorsManager {
   static const Color primaryColor = Color(0xFF1F1338);
   static const Color secondaryColor = Color(0xFF2A2A2E);
   static const Color buttonColor = Color(0xFF7e64ff);
+  static const Color appBarColor = Color(0xff503ac2);
+  static const Color EdittextFieldColor = Color(0xff372d67);
   static const Color textFieldColor = Color(0xFFccc2fe);
+
+
 }
